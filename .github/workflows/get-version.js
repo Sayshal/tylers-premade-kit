730c1859563c21029284a6cd00a43d8cb7b74420
Tyler <3deathsaves@gmail.com>
2025-08-31 14:26:37 -0600

    Update build scripts

--- .github/workflows/get-version.js
+++ .github/workflows/get-version.js
@@ -1,3 +1,10 @@
-// Thanks gambit (github.com/gambit07) for letting me test with his workflow!
-let fs = require('fs')
-console.log(JSON.parse(fs.readFileSync('./module.json', 'utf8')).version)
+let fs = require('fs');
+const moduleJson = JSON.parse(fs.readFileSync('./module.json', 'utf8'));
+
+console.log('VERSION:' + moduleJson.version);
+console.log('COMPAT_MIN:' + moduleJson.compatibility.minimum);
+console.log('COMPAT_VERIFIED:' + moduleJson.compatibility.verified);
+console.log('COMPAT_MAX:' + moduleJson.compatibility.maximum);
+console.log('MODULE_ID:' + moduleJson.id);
+console.log('MODULE_TITLE:' + moduleJson.title);
+console.log('MODULE_URL:' + moduleJson.url);
